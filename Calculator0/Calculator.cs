b9fa472225210c118f2a481374be187db40a2d36
ozcanea <ekinanilozcan@gmail.com>
2022-01-21 09:26:53 +0300

    Add project files.

---
+++ Calculator0/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator0
+{
+    public partial class FormCalculator : Form
+    {
+        public FormCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            lblPrint.Text += b.Text;
+        }
+    }
+}
